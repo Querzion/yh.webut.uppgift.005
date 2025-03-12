9b08f73c43e60beb5d93a6150771b8c5300f0793
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-12 16:25:33 +0100

    merged branch controllers. created two controllers specified in the tips & trix video.

--- assignment_alpha/Presentation.WebApp/obj/Debug/net9.0/Presentation.WebApp.AssemblyInfo.cs
+++ assignment_alpha/Presentation.WebApp/obj/Debug/net9.0/Presentation.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41497529c8eaa1e7df92ad857a56a65f45730a79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64b6c395835088991b87e72ea164d125d68abec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
