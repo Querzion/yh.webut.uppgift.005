0770394b20bffd82697f71f98326b9d6204a9905
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-12 21:13:59 +0100

    obj folder is being irritating!

--- assignment_alpha/Presentation.WebApp/obj/Debug/net9.0/Presentation.WebApp.AssemblyInfo.cs
+++ assignment_alpha/Presentation.WebApp/obj/Debug/net9.0/Presentation.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb15184225268c36addb1ce82744ae428549b43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dfc40161e884286b3555fe82e2219401810b225")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
