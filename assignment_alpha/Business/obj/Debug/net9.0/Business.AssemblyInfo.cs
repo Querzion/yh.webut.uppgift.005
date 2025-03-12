fa51c03352465f6e87e64ef34d5139d8d756f476
minhje <mminnahjelm@gmail.com>
2025-03-12 14:20:32 +0100

    merged branch. added css

--- assignment_alpha/Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
+++ assignment_alpha/Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b770038cfda6ff1dd374413bf7167aab50f7693")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada3916e40ed964ae58de8fe92a972e8774154d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- assignment_alpha/Tests.Data/obj/Debug/net9.0/Tests.Data.AssemblyInfo.cs
+++ assignment_alpha/Tests.Data/obj/Debug/net9.0/Tests.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b770038cfda6ff1dd374413bf7167aab50f7693")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada3916e40ed964ae58de8fe92a972e8774154d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
