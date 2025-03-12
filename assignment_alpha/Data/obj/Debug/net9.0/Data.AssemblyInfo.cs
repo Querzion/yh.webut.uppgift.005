456ad9b13ff187aace116ed4356bbc36cb176723
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-12 21:05:19 +0100

    Created Partials, Components & Sections folders, added existing partial components.

--- assignment_alpha/Data/obj/Debug/net9.0/Data.AssemblyInfo.cs
+++ assignment_alpha/Data/obj/Debug/net9.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcf0e4eff30444c077db444a6d81af914945643c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1cfd7cbdb4defc21f7366b0713b9b1ee20c718a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
