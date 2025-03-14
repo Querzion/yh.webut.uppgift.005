6498447b7d36265c62804465fb3f458e5476951d
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-14 11:11:28 +0100

    It doesn't seem like the .gitignore is works properly. These obj debug changes are irritating to be forced to update.

--- assignment_alpha/Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
+++ assignment_alpha/Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7daabdd17ebe9e06a670a4fdd8a2636ab20d02e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fa00c6d8156e8569268ad6c0b3961067784cc81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
