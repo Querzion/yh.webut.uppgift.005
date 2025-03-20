a0d8830a11f4e1f62404ad28aa721222f113e195
minhje <mminnahjelm@gmail.com>
2025-03-20 15:09:30 +0100

    sin in form

--- assignment_alpha/Presentation.WebApp/Models/LoginViewForm.cs
+++ assignment_alpha/Presentation.WebApp/Models/LoginViewForm.cs
@@ -9,9 +9,12 @@ public class LoginViewForm
     [DataType(DataType.EmailAddress)]
     public string Email { get; set; } = null!;
     
-    
     [Required(ErrorMessage = "Required")]
     [Display(Name = "Password", Prompt = "Enter a password.")]
     [DataType(DataType.Password)]
     public string Password { get; set; } = null!;
+    
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Remember Me", Prompt = "Remember me")]
+    public bool RememberMe { get; set; } = false;
 }
