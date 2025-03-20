5cb825d42498fa926635c8bcb783cf99277a6ace
minhje <mminnahjelm@gmail.com>
2025-03-20 14:36:27 +0100

    g

--- assignment_alpha/Business/Models/MemberSignIn.cs
+++ assignment_alpha/Business/Models/MemberSignIn.cs
@@ -1,6 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Business.Models;
 
 public class MemberSignIn
 {
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Email", Prompt = "Your email address.")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+
+
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Password", Prompt = "Enter your password.")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
     
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Remember Me", Prompt = "Remember me")]
+    public bool RememberMe { get; set; } = false;
 }
