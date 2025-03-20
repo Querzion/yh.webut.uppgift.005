1a608aa96cfeb429de4d2694bf723e157680e4f7
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-20 14:28:22 +0100

    Created Member Sign In Form and Sign Up Form.

---
+++ assignment_alpha/Presentation.WebApp/Models/MemberSignInForm.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.WebApp.Models;
+
+public class MemberSignInForm
+{
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Email Address", Prompt = "Enter your email address.")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+    
+    
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Password", Prompt = "Enter a password.")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+}
---
+++ assignment_alpha/Presentation.WebApp/Models/MemberSignUpForm.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.WebApp.Models;
+
+public class MemberSignUpForm
+{
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "First Name", Prompt = "Enter your first name.")]
+    [DataType(DataType.Text)]
+    public string FirstName { get; set; } = null!;
+    
+    
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Last Name", Prompt = "Enter your last name.")]
+    [DataType(DataType.Text)]
+    public string LastName { get; set; } = null!;
+    
+    
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Email Address", Prompt = "Enter your email address.")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", 
+        ErrorMessage = "Invalid Email")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+    
+    
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Password", Prompt = "Enter a password.")]
+    [RegularExpression(@"^(?=.*[A-Ö])(?=.*[a-ö])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$", 
+        ErrorMessage = "Invalid Password")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+    
+    
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Confirm Password", Prompt = "Confirm password.")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; } = null!;
+    
+    
+    [Required(ErrorMessage = "Required")]
+    [Display(Name = "Terms & Conditions", Prompt = "I Accept the terms & conditions.")]
+    public bool TermsAndConditions { get; set; }
+}
