bf814ed84fa9b94cb781564efa7cf8369b9cf15a
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-21 08:53:11 +0100

    Created the Add / Edit View Models / Models, for the respective modals (Project, Member, Client).

---
+++ assignment_005_alpha/Presentation.WebApp/Controllers/AddProjectViewModel.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.WebApp.Controllers;
+
+public class AddProjectViewModel
+{
+    [Display(Name = "Project Image", Prompt = "Select an image")]
+    [DataType(DataType.Upload)]
+    public IFormFile? ProjectImage { get; set; }
+    
+    [Display(Name = "Project Name", Prompt = "Enter client name")]
+    [DataType(DataType.Text)]
+    [Required(ErrorMessage = "Required")]
+    public string ProjectName { get; set; } = null!;
+    
+    [Display(Name = "Email", Prompt = "Enter email address")]
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Required")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", 
+        ErrorMessage = "Invalid email")]
+    public string Email { get; set; } = null!;
+    
+    [Display(Name = "Location", Prompt = "Enter location")]
+    [DataType(DataType.Text)]
+    public string? Location { get; set; }
+    
+    [Display(Name = "Phone Number", Prompt = "Enter phone number")]
+    [DataType(DataType.PhoneNumber)]
+    public string? PhoneNumber { get; set; }
+}
